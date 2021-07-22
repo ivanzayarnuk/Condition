fe0345680373dfd2728ef38221c46cc26972df6b
Ivan <zayarnukivan@gmail.com>
2021-07-22 18:51:44 +0300

    Condition

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num == 50) ? console.log('Ok') : console.log('Error');
+
+
+const num = '51';
+
+switch(num){
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '51':
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+
+}
